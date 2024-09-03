b06307734f973f429acb0f617a79955148986413
Gabriel Gravina <gabrielgravina01@gmail.com>
2024-09-03 13:49:35 -0300

    Utilizando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Student from "./Students";
 
 function App() {
   return (
     <>
-     
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Lucas" age={42} isStudent={false}/>
+      <Student name="Pedro" age={22} isStudent={true}/>
+      <Student name="Amanda" age={21} isStudent={true}/>
     </>
   );
 }
