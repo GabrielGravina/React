62268709ea885491c7cfbc5c36c7c5fdacd8a845
Gabriel Gravina <gabrielgravina01@gmail.com>
2024-09-03 14:07:56 -0300

    Lidando com Conditional Rendering

---
+++ src/UserGreeting.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+function UserGreeting(props){
+
+    const welcomeMessage = 
+        <h2 className="welcome-message">    
+            Welcome {props.username}
+        </h2>
+
+    const logInPrompt = 
+        <h2 className="login-prompt">
+            Please, log in to continue 
+        </h2>
+
+
+    return(props.isLoggedIn ? welcomeMessage : logInPrompt)
+                              
+}
+
+UserGreeting.proptypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+}
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest",
+}
+
+export default UserGreeting
