9fce7fe1251ecfcb4bf4741315b33242e23efb7c
Gabriel Gravina <gabrielgravina01@gmail.com>
2024-09-03 18:00:33 -0300

    Utilizando Render Lists

---
+++ src/List.jsx
@@ -0,0 +1,25 @@
+
+
+function List(  ){
+
+    const fruits = [
+        {id: 1, name:"Orange", calories: 45}, 
+        {id: 2, name:"Apple", calories: 90}, 
+        {id: 3, name:"Watermelon", calories: 75}, 
+        {id: 4, name:"Strawberry", calories: 30}, 
+        
+    ];
+
+    fruits.sort((a, b) => b.calories - a.calories);
+
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp;
+                                            {fruit.calories}</li>);
+
+
+    return (<ol>{listItems}</ol>)
+
+}
+
+export default List
